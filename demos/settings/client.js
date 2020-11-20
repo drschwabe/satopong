ea4e434877330ac7a3ba36ed749352ac0c58b21f
Derrick Schwabe <admin@nerdbusiness.com>
2020-11-20 15:49:21 -0700

    Add a techniq to merge css classes of menu items, add a separate menu example, style tweaks.

--- demos/settings/client.js
+++ demos/settings/client.js
@@ -1,5 +1,6 @@
 const $ = require('jquery')
 const _ = require('underscore')
+const _l = require('lodash')
 const _slugify = require('underscore.string/slugify')
 
 import {html,render} from 'lit-html'
@@ -16,13 +17,14 @@ document.head.insertAdjacentHTML('beforeend', `
   .font-Press-Start-2 {
     font-family: 'press_start_2pregular', monospace;
   }
+  .text-cyan { color: #b5eff1 }
 </style>`)
 
 document.body.setAttribute("style", "background-color: black;")
 
 $('body').addClass('text-center pt-10 font-Press-Start-2').prepend('<div id="menu"></div>')
 
-let menu = {
+let startMenu = {
   'SATOPONG': {
     highlighted : true, 
     classes : 'text-5xl'
@@ -36,19 +38,47 @@ let menu = {
   'EJECT SATOSHIS' : { disabled: true }
 }
 
+let connectMenu = {
+  'COPY BEACON' : {
+    classes : 'mt-20',
+    activated : {
+      show : true, 
+      extend : true, 
+      name : 'BEACON COPIED!',
+      classes : 'text-cyan',
+      selectable : false
+    }
+  },
+  'PASTE BEACON' : {},
+  '...connecting' : {},
+  'back' : {
+    classes: 'mt-10'
+  }
+}
 
 let computeTemplate = menu => {
+
+  let mergeCustomizer = (objValue, srcValue, key) => {
+    if(key === 'classes') return `${objValue} ${srcValue}`
+    return srcValue
+  }
+
   //Convert the 'user friendly' supplied object into a mechanical template-ready object: 
   menu = _.map( menu, (val, key) => {
     let item = _.extend( { name : key }, val )
     if(!item.href) item.href = 'pong/menu/' + _slugify(item.name)
+    if(item.activated && item.activated.show) {
+      item = item.activated.extend ? item = _l.mergeWith( item, item.activated, mergeCustomizer ) : item.activated
+    }
+    if(!item.classes) item.classes = ''
     return item
   }) 
   return html`
     ${  repeat( menu, item => html`
-      <a class="h1 py-2 block text-white
+      <a class="h1 block text-white
                 ${item.disabled ? 'text-opacity-25' : 'text-opacity-75 hover:text-opacity-100' }
-                ${item.classes ? item.classes : '' }"
+                ${item.classes}"
+         style="margin: 2rem 0;"
         href="${item.href}"
       >
         ${item.name}
@@ -57,5 +87,6 @@ let computeTemplate = menu => {
   `
 }
 
-render( computeTemplate(menu) , document.getElementById('menu'))
+render( computeTemplate(connectMenu) , document.getElementById('menu'))
+
 
