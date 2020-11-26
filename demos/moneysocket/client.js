442e49444454376bc8bde2559b147c56ebaeb24f
Derrick Schwabe <admin@nerdbusiness.com>
2020-11-26 16:13:21 -0700

    Poll until a moneysocket connection is established; automatically connect when moneysocket lightning node is avail

--- demos/moneysocket/client.js
+++ demos/moneysocket/client.js
@@ -1,13 +1,15 @@
 const $ = require('jquery')
 const { html, render } = require('lit-html')
 const { MoneysocketBeacon, ProviderStack } = require('moneysocket')
+const asyncJs = require('async') 
 
 //### UI state and template/rendering ####
 $('body').html( '<div id="content" class="center"></div>')
 
 const state = {
   connecting : true, 
-  connected : false
+  connected : false,
+  beaconReceived : false
 }
 
 const template = () => html`
@@ -42,6 +44,7 @@ providerStack.onstackevent = (layer_name, nexus, status)  => {
   if(status === 'NEXUS_WAITING') {
     state.connecting = false 
     state.connected = true 
+    delete state.error
 
     //share beacon with buyer: 
     state.beaconStr = new MoneysocketBeacon().toString()
@@ -53,13 +56,39 @@ providerStack.onstackevent = (layer_name, nexus, status)  => {
 }
 
 //connect to 'on-demand cloud lightning moneysocket-enabled node' :
-$.get('/moneysocketserver/beacon', body => {
-  if(!body || !body.beacon) {
-    state.error = 'no beacon :/'
-    return renderPage() 
-  } 
+const getBeacon = cb => {
+  $.get('/moneysocketserver/beacon', body => {
+    console.log('get beacon')
 
-  let serverBeacon = new MoneysocketBeacon.fromBech32Str(body.beacon)[0]
+    if(!body || !body.beacon) {
+      state.error = 'no beacon :/'
+      renderPage()
+      if(cb) return cb() 
+      return  
+    } 
 
-  providerStack.doConnect( serverBeacon )
-})
+    if(state.beaconReceived) {
+      if(cb) return cb 
+      return
+    } 
+
+    state.beaconReceived = true 
+
+    let serverBeacon = new MoneysocketBeacon.fromBech32Str(body.beacon)[0]
+
+    providerStack.doConnect( serverBeacon )
+
+    if(cb) return cb() 
+  })
+}
+
+
+asyncJs.until(
+  cb => cb(null, state.beaconReceived), 
+  cb => 
+    getBeacon( () => 
+      setTimeout(() => 
+        cb(null, state.beaconReceived)
+      , 5000)
+    )
+) 
