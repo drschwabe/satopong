722168305c505bc78363652e2bfaa0095f32f187
Derrick Schwabe <admin@nerdbusiness.com>
2020-12-03 18:36:06 -0700

    Implement ConsumerStack and its' events watching/handling to respond and update UI when buyer connects and connects a downstream satoshi source; some initial handling for disconnection too.

--- demos/moneysocket/client.js
+++ demos/moneysocket/client.js
@@ -1,7 +1,7 @@
 const $ = require('jquery')
 const { html, render } = require('lit-html')
 
-const { MoneysocketBeacon, ProviderStack, WebsocketLocation } = require('moneysocket')
+const { MoneysocketBeacon, ProviderStack, ConsumerStack, WebsocketLocation } = require('moneysocket')
 
 //constants from moneysocket reference spec: 
 const DEFAULT_HOST = "relay.socket.money"
@@ -10,6 +10,7 @@ const DEFAULT_USE_TLS = true
 
 const asyncJs = require('async') 
 const Kjua = require('kjua')
+const _ = require('underscore')
 
 //### UI state and template/rendering ####
 $('body').html( '<div id="content" class="center"></div>')
@@ -17,7 +18,8 @@ $('body').html( '<div id="content" class="center"></div>')
 const state = {
   connecting : true, 
   connected : false,
-  beaconReceived : false
+  beaconReceived : false,
+  buyerConnected : false
 }
 
 const template = () => html`
@@ -26,17 +28,28 @@ const template = () => html`
     html`<p class="orange">connecting...</p>` : ''
   }
 
-  ${ !state.connecting && state.connected ? 
+  ${ !state.connecting && state.connected && !state.buyerConnected ? 
     html`
       <p class="green bold">Seller moneysocket active!</p>
       <h3>beacon:</h3>
       <div id="beacon"></div>
-    `
-    : ''
+      <p class="break-word">${state.beaconStr}</p>
+    ` : ''
   }
 
   ${ !state.connecting && state.error ? 
     html`<p class="red">${state.error}</p>` : ''
+  }
+
+  ${ state.buyerConnected ? 
+    html`<p class="green bold">Buyer connected!</p>
+    <p>Available micro satoshis: ${state.buyerAvailableSats} </p>`
+   : ''
+  }
+
+  ${ state.buyerConnectedButNoSats ? 
+    html`<p class="orange">AWAITING BUYER'S DOWNSTREAM CONNECTION</p>`
+   : ''
   }
 `
 
@@ -47,8 +60,10 @@ renderPage()
 //### Seller app functionality ### 
 //establish moneysocket...
 const providerStack = new ProviderStack
+const consumerStack = new ConsumerStack
 
 providerStack.onstackevent = (layer_name, nexus, status)  => {
+  console.log(status)
   if(status === 'NEXUS_WAITING') {
     state.connecting = false
     state.connected = true
@@ -58,20 +73,28 @@ providerStack.onstackevent = (layer_name, nexus, status)  => {
     let sellerBeaconForBuyer = generateNewBeacon() 
     
     let beaconStr = sellerBeaconForBuyer.toString()
-    renderPage() 
+    state.beaconStr = beaconStr
 
-    console.log( beaconStr )
+    renderPage() 
 
     //convert to a QR code ... TODO
     let sellerBeaconForBuyerQr = qrCode( sellerBeaconForBuyer.toBech32Str())
   
     document.getElementById('beacon').appendChild(sellerBeaconForBuyerQr)
 
+    console.log('beacon for buyer generated & rendered')
+
+    setTimeout( () => {
+      //now connect to the same beacon we just created, but with the consumer stack....
+      consumerStack.doConnect(  sellerBeaconForBuyer  )
+      
+    }, 2000)
+
   }
   if(status === 'NEXUS_DESTROYED' || status === 'NEXUS_REVOKED') {
     state.connected = false
     state.beaconReceived = false
-    state.error = 'connection lost'
+    state.error = 'connection to seller lost'
     //show this err brierfly before starting the loop again:
     setTimeout( () => loopForBeacon, 2000)
   }
@@ -80,6 +103,36 @@ providerStack.onstackevent = (layer_name, nexus, status)  => {
 
 }
 
+consumerStack.onstackevent = (layer_name, nexus, status)  => {
+  console.log(status)
+  if(status === 'NEXUS_CREATED') {
+    if( _.isBoolean(nexus.handshake_finished) && !nexus.handshake_finished) {
+      //if the nexus obj has a property '.handshake_finished' regardless of it is false, it means the buyer has at least a connection (but no downstream connection; ie- they have not yet connected their wallet)
+      state.buyerConnectedButNoSats = true 
+    }
+  } else if(status === 'NEXUS_REVOKED' && state.buyerConnectedButNoSats) {
+    delete state.buyerConnectedButNoSats
+    state.error = 'connection to buyer lost'
+  } else if(status === 'NEXUS_REVOKED' && state.buyerConnected) {
+    state.buyerConnected = false
+    delete state.buyerAvailableSats
+    state.error = 'connection to buyer lost (was connected OK)'
+  } 
+  renderPage()
+}
+
+consumerStack.onproviderinfo = info => {
+  console.log(info)
+  //we connected!  
+  delete state.buyerConnectedButNoSats 
+  state.buyerConnected = true 
+  state.buyerAvailableSats = info.wad.msats 
+  renderPage() 
+}
+
+// consumerStack.onping = msecs => {
+//   console.log(msecs)
+// }
 
 const generateNewBeacon = () => {
   let location = new WebsocketLocation(DEFAULT_HOST, DEFAULT_PORT,DEFAULT_USE_TLS)
